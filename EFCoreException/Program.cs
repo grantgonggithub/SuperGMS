91cbb55f6b0c8fac2c7155987d96e8a5e9dc29d7
grantgong <grantgong@qt-asia.com>
2019-06-28 16:00:39 +0800

    update

--- EFCoreException/Program.cs
+++ EFCoreException/Program.cs
@@ -14,31 +14,15 @@ namespace EFCoreSameKeyException
             using (var context = new demoContext())
             {
                 var defaultPrimaryKey = "TestPK1";
-               var _item = context.Set<item>().Find(defaultPrimaryKey); // Use the primary key to query the primary record
-                var query = context.Set<itemDetail>().Where<itemDetail>(x => x.ItemGID == _item.ItemGID).AsEnumerable();
-                context.Set<itemDetail>().RemoveRange(query); // Delete all details based on foreign keys
-                var _itemDetails = _item.itemDetail.Where(x => x.LineId % 2 == 0).ToList(); // Select records where lineid is even
-                int i = 0;
-                for (i=0;i< _itemDetails.Count(); i++)
-                {
-                    var d = new itemDetail {
-                        GUID = Guid.NewGuid().ToString("N"),
-                        ItemGID = _itemDetails[i].ItemGID,
-                        LineId = i,
-                        LineName= _itemDetails[i].LineName
+                var _item = context.Set<item>().Find(defaultPrimaryKey); // query all details
+              
+                context.Set<item>().Remove(_item); // Delete all details based on foreign keys
+            
+                var d = new item {
+                        ItemGID = _item.ItemGID,
                     };
-                    context.Set<itemDetail>().Add(d); //Insert records whose lineid is even after changing their Numbers
-                }
-
-                var newOne = new itemDetail
-                {
-                    GUID = Guid.NewGuid().ToString("N"),
-                    ItemGID = defaultPrimaryKey,
-                    LineId = i+1,
-                    LineName ="new_LineName"
-                };
-                context.Set<itemDetail>().Add(newOne); //Insert new detail
-
+                    context.Set<item>().Add(d);
+               
                 context.SaveChanges(); // commit
             }
         }
