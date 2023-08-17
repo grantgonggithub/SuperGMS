95b51fab0388e5dbd9141613d9e0a33c947cb57e
gongjianchun <nnn987@126.com>
2023-08-17 17:03:59 +0800

    增加websocket代理类

--- SuperGMS.WebSocketService/SendMessageToUser.cs
+++ SuperGMS.WebSocketService/SendMessageToUser.cs
@@ -9,16 +9,16 @@ namespace WebSocketService
     /// <summary>
     /// 给指定的WebSocket客户端发送消息
     /// </summary>
-    public class SendMessageToUser : RpcBaseServer<EventMsg<string>, Nullables>
+    public class SendMessageToUser : RpcBaseServer<EventMsgEx, Nullables>
     {
-        protected override Nullables Process(EventMsg<string> valueArgs, out StatusCode code)
+        protected override Nullables Process(EventMsgEx valueArgs, out StatusCode code)
         {
             code = StatusCode.OK;
             SuperWebSocketManager.SendMessage(valueArgs);
             return Nullables.NullValue;
         }
 
-        protected override bool Check(EventMsg<string> args, out StatusCode code)
+        protected override bool Check(EventMsgEx args, out StatusCode code)
         {
             if (Context.Args.ct == ClientType.InnerRpc.ToString()||base.CheckLogin(args,out code))
             {
