1a87f7de0aefd5a24dc677f99aa8f8de2f1cbd7e
gongjianchun <406333743@126.com>
2025-08-20 17:40:02 +0800

    提交项目

--- SuperGMS/DB/EFEx/MyDbFactory/SqlRepositoryManager.cs
+++ SuperGMS/DB/EFEx/MyDbFactory/SqlRepositoryManager.cs
@@ -10,15 +10,16 @@
  功能描述：
 
 ----------------------------------------------------------------*/
+using Dm.filter;
 using Dm.util;
 using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SqlSugar;
-using SuperGMS.DB.EFEx.CrudRepository;
 using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
-using System.Text;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SuperGMS.DB.EFEx.GrantDbFactory
 {
@@ -27,6 +28,7 @@ namespace SuperGMS.DB.EFEx.GrantDbFactory
     /// </summary>
     public class SqlRepositoryManager
     {
+        private static ILogger logger = SuperGMS.Log.LogFactory.CreateLogger<SqlRepositoryManager>();
         public static DbBase GetSqlRepository(DbInfo dbInfo)
         {
             DbBase db = null;
@@ -57,14 +59,31 @@ namespace SuperGMS.DB.EFEx.GrantDbFactory
                 ConfigId = dbInfo.toString().GetHashCode(),
                 ConnectionString = getDbConnectionString(dbInfo),
                 DbType = getSqlSugarDbType(dbInfo),
-                IsAutoCloseConnection=false,
+                IsAutoCloseConnection = false,
+            }, sqlInfo => {
+                sqlInfo.Aop.OnLogExecuting = (sql, para) => {
+                    logger.LogInformation(UtilMethods.GetNativeSql(sql, para));
+                };
             });
-            client.Ado.ExecuteCommand($"USE {dbInfo.DbName};");
+            doSomePreProcess(client, dbInfo);
             return client;
         }
 
         private static SqlSugar.DbType getSqlSugarDbType(DbInfo dbInfo) {
             return (SqlSugar.DbType)dbInfo.DbType;
+        }
+
+        private static void doSomePreProcess(ISqlSugarClient client, DbInfo dbInfo)
+        {
+            switch (dbInfo.DbType)
+            {
+                case DbType.Doris:
+                    client.Ado.ExecuteCommand($"USE {dbInfo.DbName};");
+                    break;
+                default:
+                    break;
+
+            }
         }
 
         private static string getDbConnectionString(DbInfo dbInfo)
@@ -76,10 +95,19 @@ namespace SuperGMS.DB.EFEx.GrantDbFactory
                 DbType.Oracle => OracleDBContextOptionBuilder.GetDbConnectionString(dbInfo),
                 DbType.PostgreSQL => PostgresqlDBContextOptionBuilder.GetDbConnectionString(dbInfo),
                 DbType.Doris => $"server={dbInfo.Ip};port={dbInfo.Port};database={dbInfo.DbName};user={dbInfo.UserName};password={dbInfo.Pwd};Pooling=true;LoadBalance=RoundRobin;",
-                DbType.MongoDb => dbInfo.Ip.indexOf(":") < 0 ? $"mongodb://{dbInfo.UserName}:{dbInfo.Pwd}@{dbInfo.Ip}:{dbInfo.Port}/{dbInfo.DbName}?authSource=admin" : $"mongodb://{dbInfo.UserName}:{dbInfo.Pwd}@{dbInfo.Ip}/{dbInfo.DbName}?authSource=admin&replicaSet={dbInfo.Other}",// var str="mongodb://root:123456@117.72.212.3:27017,117.72.212.4:27017,117.72.212.5:27017/testDB?authSource=admin&replicaSet=rs0";//117.72.212.3:27017,117.72.212.4:27017,...：多个 MongoDB 节点地址。 //testDB：你要连接的数据库。 //authSource=admin：认证数据库是 admin。 //replicaSet=rs0：副本集名称必须写对（和你 MongoDB 实际副本集名字一致）。
+                DbType.MongoDb => mongoDbConnectionString(dbInfo),
                 DbType.Sqlite => new SqliteConnectionStringBuilder() { DataSource = Path.Combine(Environment.CurrentDirectory, dbInfo.DbName), Mode = SqliteOpenMode.ReadWriteCreate, Password = dbInfo.Pwd }.toString(),
-                _ =>  throw new Exception($"database.config中配置了不支持的数据库类型{dbInfo.DbType}"),
+                _ => throw new Exception($"database.config中配置了不支持的数据库类型{dbInfo.DbType}"),
             };
+        }
+
+        private static string mongoDbConnectionString(DbInfo dbInfo)
+        {
+            var otherInfo = string.IsNullOrWhiteSpace(dbInfo.Other) ?string.Empty: ";replicaSet=" + dbInfo.Other;
+            //host = 222.71.212.3; Port = 27017; Database = testDB; Username = root; Password = 123456; authSource = admin; replicaSet =
+            // var str="mongodb://root:123456@117.72.212.3:27017,117.72.212.4:27017,117.72.212.5:27017/testDB?authSource=admin&replicaSet=rs0";//117.72.212.3:27017,117.72.212.4:27017,...：多个 MongoDB 节点地址。 //testDB：你要连接的数据库。 //authSource=admin：认证数据库是 admin。 //replicaSet=rs0：副本集名称必须写对（和你 MongoDB 实际副本集名字一致）。
+            //return dbInfo.Ip.indexOf(":") < 0 ? $"mongodb://{dbInfo.UserName}:{dbInfo.Pwd}@{dbInfo.Ip}:{dbInfo.Port}/{dbInfo.DbName}?authSource=admin{otherInfo}" : $"mongodb://{dbInfo.UserName}:{dbInfo.Pwd}@{dbInfo.Ip}/{dbInfo.DbName}?authSource=admin{otherInfo}";
+            return $"host={dbInfo.Ip};Port={dbInfo.Port};Database={dbInfo.DbName};Username={dbInfo.UserName};Password={dbInfo.Pwd};authSource=admin{otherInfo}";
         }
     }
 }
